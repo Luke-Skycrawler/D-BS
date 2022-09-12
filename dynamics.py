4fa85b5606f438b0b984528f34f10a7198800144
Shay P.C <3180102686@zju.edu.cn>
2022-09-11 23:59:54 -0600

    add psudo

--- dynamics.py
+++ dynamics.py
@@ -12,18 +12,27 @@ dim = 2
 n_elements = n - 2 * (order_k - 1)
 alpha, beta = 35, 10
 mu = 30
-gravity = 9.8
+gravity = ti.Vector([0.0, -9.8])
 dt = 8e-3
+p_like = lambda: ti.Vector.field(dim, float) 
 p = ti.Vector.field(dim, float)
-container = ti.root.pointer(ti.i, 10).pointer(ti.i, 10)
-container.place(p)
-element = ti.root.dense(ti.i, n_elements)
-x = ti.Vector.field(2, float)
-v = ti.Vector.field(2, float)
-J = ti.Matrix.field(order_k, order_k, float)
-M = ti.Matrix.field(order_k, order_k, float)
-K = ti.Matrix.field(order_k, order_k, float)
-element.place(x, v, alpha, beta, mu, J)
+r, d, b, v, old_p = [p_like() for _ in range(5)]
+# for cg
+
+# container = ti.root.pointer(ti.i, 10).pointer(ti.i, 10)
+container = ti.dense(ti.i, n)
+container.place(p, r, d, b, v, old_p)
+
+element = ti.root.dense(ti.i, n_elements).dense(ti.j, order_k)
+
+scalar = lambda: ti.field(float)
+J_diag = scalar()
+Ju_diag = scalar()
+Juu_diag = scalar()
+# remember those are 3 * 3 matrix, i.e., identity multiplying the scalar value 
+tmp = ti.Vector.field(dim, float)
+Ap_ret = ti.Vector.field(dim, float)
+element.place(J_diag, Ju_diag, Juu_diag, tmp, Ap_ret)
 
 # TODO: for node with multiplicity, add up coeffcients
 @ti.func
@@ -32,8 +41,8 @@ def J_ii(u, i):
     if i == 0:
         for j in ti.static(range(-order_k + 1, 1)):
             ret += open_basis_3(j, u, n)
-    elif i == n:
-        for j in ti.static(range(n, n + order_k - 2)):
+    elif i == n - order_k:
+        for j in ti.static(range(n - order_k, n)):
             ret += open_basis_3(j, u, n)
     else :
         ret = open_basis_3(i, u, n) 
@@ -48,8 +57,8 @@ def JuTJu_ii(u, i):
     if i == 0:
         for j in ti.static(range(-order_k + 1, 1)):
             ret += dB_3(j, u, n)
-    elif i == n:
-        for j in ti.static(range(n, n + order_k - 2)):
+    elif i == n - order_k:
+        for j in ti.static(range(n - order_k, n)):
             ret += dB_3(j, u, n)
     else :
         ret = dB_3(i, u, n) 
@@ -61,8 +70,8 @@ def JuuTJuu_ii(u, i):
     if i == 0:
         for j in ti.static(range(-order_k + 1, 1)):
             ret += d2B_3(j, u, n)
-    elif i == n:
-        for j in ti.static(range(n, n + order_k - 2)):
+    elif i == n - order_k:
+        for j in ti.static(range(n - order_k, n)):
             ret += d2B_3(j, u, n)
     else :
         ret = d2B_3(i, u, n) 
@@ -84,8 +93,17 @@ def jacobian(e):
         # mass
         K[e][I] = alpha * quad_JuTJu_ii(i) + beta * quad_JuuTJuu_ii(i)
         # stiffness
-        
     # return J[e]
+    
+@ti.kernel
+def compute_b():
+    '''
+    b = 4\triangle t ^ 2 f_p + 8Mv - (3M - 2\triangle t D)p^{(t-\triangle t)}-\int \mu J^Tc^{(t - \triangle t)} du
+
+    '''
+    for i in range(n_elements):
+        4 * dt ** 2 * gravity + 8 * Mp(v) - 3 * Mp(p_m1) - mu * quad()
+    pass
 
 @ti.func
 def mass(e):
@@ -104,3 +122,53 @@ def iga():
         # M = mass(e)
         # D = damping(e)
         # K = stiffness(e)
+
+@ti.func
+def Ap(p):
+    '''
+    returns matrix-vector product with A
+    A = 4M + 2 \triangle t D + 4(\triangle t) ^ 2 K
+    '''
+    ret = ti.Vector([0.0] * n * dim)
+    # FIXME: probably need a field
+    for e in range(n_elements):
+        # for each element, integrate M = J^T Jp, K = alpha Ju^T Ju p + beta Juu^T Juu p
+        
+        # compute v = J(u)p
+        for k in ti.static(range(order_k)):
+            J_diag[e, k] = J_ii(k + e, u) 
+            tmp[e, k] = J_ii(k + e, u) * p[i + k]
+            # compute J^T v
+            Ap_ret[e, k] += J_diag(e, k) * tmp[e, k]
+        # integrate 
+
+        pass
+
+@ti.func
+def Mp(p):
+    '''
+    product with mass matrix M
+    '''
+    pass
+@ti.kernel
+def cg(p):
+    # informally written
+    d = r = b - Ap(p)
+    for i in range(n):
+        alpha = r.dot(r) / d.dot(Ap(d))
+        p += alpha * d
+        tmp_r = r
+        r -= alpha * Ap(d)
+        beta = r.dot(r) / tmp_r.dot(tmp_r)
+        d = r + beta * d
+        
+    
+def step():
+    compute_b()
+    _b = b.to_numpy()
+    # solver = ti.linalg.SparseSolver(solver_type="LDLT")
+    # solver.analyze_pattern(A)
+    # solver.factorize(A)
+    # _p = solver.solve(_b)
+    p.from_numpy(_p.reshape((dim, n))) # update p
+    
